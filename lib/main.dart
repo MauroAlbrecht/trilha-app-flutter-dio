cb5e67105064ec5a57a22b7d7f3a62bc986cf2f0
Mestre Dos Lances <appmestredoslances@gmail.com>
2023-10-02 11:25:24 -0300

    add dependencia do sqflite para usar o SQLite

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:trilhaapp/model/dados_cadastrais_model.dart';
 import 'package:trilhaapp/model/tarefa_hive_model.dart';
 import 'package:trilhaapp/pages/my_app.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:path/path.dart' as path;
 
-void main() async{
-
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   var documentsDirectory = await path_provider.getApplicationDocumentsDirectory();
   Hive.init(documentsDirectory.path);
   Hive.registerAdapter(DadosCadastraisModelAdapter());
   Hive.registerAdapter(TarefaHiveModelAdapter());
+  await iniciarBancoDeDados();
   runApp(const MyApp());
 }
+
+Future iniciarBancoDeDados() async {
+  return await openDatabase(path.join(await getDatabasesPath(), 'app.db'),
+      version: scripts.length,
+      onCreate: (Database db, int index) async {
+    for (var i = 1; i < scripts.length; i++) {
+      await db.execute(scripts[i]!);
+    }
+  }, onUpgrade: (Database db, int oldVersion, int newVersion) async {
+    for (var i = oldVersion + 1; i < scripts.length; i++) {
+      await db.execute(scripts[i]!);
+    }
+  });
+}
+
+Map<int, String> scripts = {
+  1: ''' CREATE TABLE tarefas (
+          id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          descricao TEXT,
+          concluido INTEGER
+  );'''
+};
